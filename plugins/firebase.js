4d650a0d3f1bc0287be50de12e44413e81b19c65
clarke newsham <clarkenewsham@clarkes-mbp-2.mynet>
2021-04-01 18:39:47 +0100

    Google firebase configuration

---
+++ plugins/firebase.js
@@ -0,0 +1,12 @@
+// MY API KEY DONT DELETE THIS FIND OUT HOW TO INTILIASE THE FIREBASE SERVICE THROUGH NUXT CONFIG FILE
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyAtX6Vqpj5sOANjhsQjk6RqxQXMtQxFLIY',
+//   authDomain: 'final-year-project-vle-webapp.firebaseapp.com',
+//   projectId: 'final-year-project-vle-webapp',
+//   storageBucket: 'final-year-project-vle-webapp.appspot.com',
+//   messagingSenderId: '1062916108812',
+//   appId: '1:1062916108812:web:cb7835cbb982e9ab7ff429',
+//   measurementId: 'G-JMY61S9TZ2',
+// }
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig)
