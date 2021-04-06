2c7516fcbcd6f753d4828403080b22265f45741f
clarke newsham <clarkenewsham@clarkes-mbp-2.mynet>
2021-04-06 17:42:04 +0100

    navbar and footer component refactor

---
+++ components/TheFooter.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer>
+    <Logo id="logo" />
+    <NuxtLink to="/">return home</NuxtLink>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+}
+</script>
+
+<style scoped>
+footer {
+  display: flex;
+  justify-content: space-around;
+  border-top: 1px solid #2d3748;
+  padding: 30px 20px;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 15px;
+}
+
+ul li:first-child {
+  padding-bottom: 10px;
+}
+
+footer a {
+  color: #e2e8f0;
+}
+
+@media only screen and (max-width: 1024px) {
+  footer {
+    flex-direction: column;
+    text-align: center;
+  }
+}
+</style>
